b3227ea5dfab777b895fd099a4d91a704df9fd4c
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 22:41:29 +0530

    [step-4]-unit test created for single custom delimiter which fails

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,6 +1,7 @@
 package com.incubyte;
 
 import java.util.Arrays;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
@@ -8,6 +9,7 @@ public class StringCalculator {
         if (input == null || input.isEmpty()) return 0;
 
         input = input.replaceAll("\\n", ",");
+
 
         return Arrays.stream(input.split(","))
                 .mapToInt(Integer::parseInt)
--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -25,4 +25,11 @@ class StringCalculatorTest {
     public void shouldSupportNewlineDelimiter() {
         assertEquals(6, StringCalculator.add("1\n2,3"));
     }
+
+    @Test
+    public void shouldSupportSingleCustomDelimiter() {
+        assertEquals(3, StringCalculator.add("//;\n1;2"));
+    }
+
+
 }
