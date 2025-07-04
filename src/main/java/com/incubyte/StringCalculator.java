f39765270251d9d32bab8a0c62e390124d6b7bf2
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 20:18:08 +0530

    [step-1]-added minimal code to pass unit test for empty string

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -2,4 +2,8 @@ package com.incubyte;
 
 public class StringCalculator {
 
+    public static int add(String input) {
+        if (input == null || input.isEmpty()) return 0;
+        return -1; // temp placeholder
+    }
 }
