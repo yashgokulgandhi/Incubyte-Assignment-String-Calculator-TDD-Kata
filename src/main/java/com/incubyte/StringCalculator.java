cb47e8daa941be3cd8145fa19577f76974296a12
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 20:38:23 +0530

    [step-2]-added minimal code to pass unit test for adding two comma seperated numbers

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,9 +1,16 @@
 package com.incubyte;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public static int add(String input) {
-        if (input == null || input.isEmpty()) return 0;
-        return -1; // temp placeholder
+        if (input == null || input.isEmpty())
+            return 0;
+
+
+        return Arrays.stream(input.split(","))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
