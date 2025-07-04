0158592b67ce0a05cfe10c868a5d4968edeef8cd
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 22:56:32 +0530

    [step-5]-added minimal code to pass unit for negative numbers

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -1,12 +1,15 @@
 package com.incubyte;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
     public static int add(String input) {
         if (input == null || input.isEmpty()) return 0;
+
 
         input = input.replaceAll("\\n", ",");
 
@@ -14,6 +17,14 @@ public class StringCalculator {
             String delim = Pattern.quote(input.substring(2, 3));
             input = input.substring(4);
             return Arrays.stream(input.split(delim)).mapToInt(Integer::parseInt).sum();
+        }
+
+        List<Integer> list = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+        List<Integer> negatives = list.stream().filter(n -> n < 0).collect(Collectors.toList());
+        if (!negatives.isEmpty()) {
+            throw new IllegalArgumentException("Negatives not allowed: " + negatives);
         }
 
         return Arrays.stream(input.split(","))
