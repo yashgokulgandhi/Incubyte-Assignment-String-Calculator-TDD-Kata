13d15669e412d1d4a86d917bfe4658b06a145c19
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 22:40:31 +0530

    [step-3]-added minimal code to pass unit test for adding new line deliminator

--- src/main/java/com/incubyte/StringCalculator.java
+++ src/main/java/com/incubyte/StringCalculator.java
@@ -7,6 +7,7 @@ public class StringCalculator {
     public static int add(String input) {
         if (input == null || input.isEmpty()) return 0;
 
+        input = input.replaceAll("\\n", ",");
 
         return Arrays.stream(input.split(","))
                 .mapToInt(Integer::parseInt)
