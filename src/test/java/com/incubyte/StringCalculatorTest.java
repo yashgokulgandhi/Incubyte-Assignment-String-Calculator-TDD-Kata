674548a05b678a7a58dc02ab96287eeed9140ba2
YashGokulgandhi <yashgokulgandhi6@gmail.com>
2025-07-04 20:17:11 +0530

    [step-1]-unit test created for empty string which fails

--- src/test/java/com/incubyte/StringCalculatorTest.java
+++ src/test/java/com/incubyte/StringCalculatorTest.java
@@ -5,5 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class StringCalculatorTest {
 
-
+    @Test
+    void emptyStringShouldReturnZero() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""), "Empty string should return 0");
+    }
 }
